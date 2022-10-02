377ddde00dd94d4edf334de6d4b177791db36c20
unsnruu <unsnruu@gmail.com>
2022-10-02 14:52:02 +0900

    🚚 firbase.ts를 api에서 config 폴더로 이를 (#4)
    firebase.ts를 api에서 config로 이동시켰습니다.
    또한 역할을 명시적으로 지칭하기 위하여 firebase.config.ts로 변경하였습니다.

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const {
   REACT_APP_API_KEY,
   REACT_APP_AUTH_DOMAIN,
@@ -24,5 +22,7 @@ const firebaseConfig = {
   measurementId: REACT_APP_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
