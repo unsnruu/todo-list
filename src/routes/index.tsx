e834b5615d29e7cafc62442c5a5931a18e34b257
unsnruu <unsnruu@gmail.com>
2022-10-05 02:23:49 +0900

    ✨ path alias 적용 (#5)
    routes 폴더로 연결되는 path alias를 import문에 적용하였습니다.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import {
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
-import Login from "./auth/Login";
+import Login from "@routes/auth/Login";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<Login />}></Route>)
