0b53adf039fe502dc1496fbd37a78cad21e08c02
unsnruu <unsnruu@gmail.com>
2022-10-05 01:13:32 +0900

    ✨ 콜렉션을 생성하는 함수 구현 (#4)
    콜렉션을 생성하는 함수를 따로 빼내었습니다.
    https://javascript.plainenglish.io/using-firestore-with-typescript-in-the-v9-sdk-cf36851bb099를 참고.

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -2,10 +2,20 @@ import { firebaseConfig } from "../config/firebase.config";
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {
+  getFirestore,
+  DocumentData,
+  CollectionReference,
+  collection,
+  doc,
+} from "firebase/firestore";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db };
+const createCollection = <T = DocumentData>(collectionName: string) => {
+  return collection(db, collectionName) as CollectionReference<T>;
+};
+
+export { app, auth, db, createCollection };
