8fb83276f6e089071f625a2f869b51db94cf7d63
unsnruu <unsnruu@gmail.com>
2022-10-05 04:32:23 +0900

    ✅ UI 관련 테스트 통과 (#5)

--- src/routes/auth/Login.tsx
+++ src/routes/auth/Login.tsx
@@ -1,9 +1,18 @@
 import * as React from "react";
+import { Form } from "react-router-dom";
 
 function Login() {
   return (
     <div>
-      <h1>Login</h1>
+      <h1>로그인</h1>
+      <Form method="post" aria-label="로그인">
+        <input type="text" />
+        <label>
+          비밀번호
+          <input type="password" />
+        </label>
+        <button>제출</button>
+      </Form>
     </div>
   );
 }
