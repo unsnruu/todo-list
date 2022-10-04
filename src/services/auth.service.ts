ba5344a04d2d2e42a9aa525518815c8b70bedacd
unsnruu <unsnruu@gmail.com>
2022-10-04 14:42:06 +0900

    ⚰️ 사용하지 않는 코드 제거 (#4)
    서버에서 받아오는 userCrendential 변수를 사용하지 않아 삭제하였습니다.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -7,23 +7,15 @@ import { auth } from "./firebase.service";
 const authService = {
   async signUp(email: string, password: string) {
     try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
+      await createUserWithEmailAndPassword(auth, email, password);
     } catch (err) {
       console.error(err);
     }
   },
   async logIn(email: string, password: string) {
     try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      console.log(userCredential);
+      await signInWithEmailAndPassword(auth, email, password);
+      // console.log(userCredential);
     } catch (err) {
       console.log(err);
     }
