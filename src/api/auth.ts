8a62f07aaf96ba68072f6c941c5d3e052f314f97
unsnruu <unsnruu@gmail.com>
2022-10-04 14:44:56 +0900

    🚚 services 폴더 네이밍 변경에 따라서 import 구문 변경 (#4)

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,4 @@
-import authService from "../services/auth";
+import authService from "../services/auth.service";
 
 const signUp = (email: string, password: string) => {
   authService.signUp(email, password);
