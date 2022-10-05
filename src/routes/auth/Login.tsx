d0f8f545ef364a09deb7c9552f9f2c8f42a96146
unsnruu <unsnruu@gmail.com>
2022-10-05 22:16:01 +0900

    ✅ 테스트에 맞추어 이메일 input의 label 생성 (#5)

--- src/routes/auth/Login.tsx
+++ src/routes/auth/Login.tsx
@@ -6,7 +6,10 @@ function Login() {
     <div>
       <h1>로그인</h1>
       <Form method="post" aria-label="로그인">
-        <input type="text" />
+        <label>
+          이메일
+          <input type="text" />
+        </label>
         <label>
           비밀번호
           <input type="password" />
