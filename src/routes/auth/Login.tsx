64aaf89cff15b43f885eb9149c99ca2d1dab08a6
unsnruu <unsnruu@gmail.com>
2022-10-09 23:23:41 +0900

    ✨ action 함수 생성 (#10)
    action 함수를 생성하고 api를 사용해 로직을 짰습니다.
    다만 firebase를 사용할 때 react-router의 이런식의 작동이 유용한지 의문이 듭니다.

--- src/routes/auth/Login.tsx
+++ src/routes/auth/Login.tsx
@@ -1,5 +1,14 @@
 import * as React from "react";
-import { Form } from "react-router-dom";
+import { Form, ActionFunctionArgs } from "react-router-dom";
+import { logIn } from "@api/auth";
+
+export async function action({ request, params }: ActionFunctionArgs) {
+  let formData = await request.formData();
+  //? 과연 이런 식의 코드가 파이어 베이스와 유용하게 쓰일 수 있을까라는 의문이 든다.
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  return logIn({ email, password });
+}
 
 function Login() {
   return (
@@ -8,11 +17,11 @@ function Login() {
       <Form method="post" aria-label="로그인">
         <label>
           이메일
-          <input type="text" />
+          <input type="text" name="email" />
         </label>
         <label>
           비밀번호
-          <input type="password" />
+          <input type="password" name="password" />
         </label>
         <button>제출</button>
       </Form>
