41cc3d3fdc456ca1456b28ab1f0eb2cbc496c578
unsnruu <unsnruu@gmail.com>
2022-10-10 01:59:32 +0900

    💡 todo 주석 추가 (#10)
    이후에 유저정보를 확인하여서 어디로 페이지를 라우팅할 지 결정해야한다는 todo를 써놨습니다.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import {
 
 import Login, { action as loginAction } from "@routes/auth/Login";
 
+//todo: auth obeserver로 element 삼항 연산!
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/login" element={<Login />} action={loginAction}></Route>
