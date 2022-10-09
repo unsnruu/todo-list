90dc4dfcec509b823cd2478953c889eb2dd88858
unsnruu <unsnruu@gmail.com>
2022-10-09 23:27:54 +0900

    ✨ action 함수를 route에 설정
    routes/auth/Login.tsx에서 설정한 action 함수를 import하여
    route의 action prop으로 전달하였습니다.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,10 @@ import {
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
-import Login from "@routes/auth/Login";
+import Login, { action as loginAction } from "@routes/auth/Login";
 
 export const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Login />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<Login />} action={loginAction}></Route>
+  )
 );
