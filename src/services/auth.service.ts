60e46c92bf4b1810b91ace933e918cc615e79b0e
unsnruu <unsnruu@gmail.com>
2022-10-11 14:15:24 +0900

    ⚰️ 불필요한 주석 제거 (#10)
    해당 주석들을 README.md로 옮겼기 때문에 불필요하여 삭제하였습니다.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,6 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase.service";
 
-//todo: ContextAPI 혹은 Redux로 user정보에 대한 관리?
-//todo: cache를 어떻게 할지?
 const authService = {
   async isLoggedIn() {
     onAuthStateChanged(auth, (user) => {
