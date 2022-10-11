0c050b7b393122f71e65d0b116791a83b6c4520f
unsnruu <unsnruu@gmail.com>
2022-10-11 16:19:38 +0900

    ✅ 테스트를 통과하게끔 UI를 구현
    테스트를 통과하도록 UI를 구현하였습니다.
    또한 추가적으로 action 함수를 생성하였습니다.

--- src/routes/auth/SignUp.tsx
+++ src/routes/auth/SignUp.tsx
@@ -1,7 +1,22 @@
+import { Form } from "react-router-dom";
+
+export async function action() {}
+
 function SignUp() {
   return (
     <div>
-      <h1>Sign Up</h1>
+      <h1>회원가입</h1>
+      <Form aria-label="회원가입">
+        <label>
+          이메일
+          <input type="text" name="d" />
+        </label>
+        <label>
+          비밀번호
+          <input type="password" name="password" />
+        </label>
+        <button>제출</button>
+      </Form>
     </div>
   );
 }
