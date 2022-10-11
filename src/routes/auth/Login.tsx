35c447644593e9625fe04edb066b6469e8c3c031
unsnruu <unsnruu@gmail.com>
2022-10-11 13:40:25 +0900

    ⚰️ 사용되지 않는 parameter 제거 (#10)
    params parameter가 사용되지 않으므로 삭제하였습니다.

--- src/routes/auth/Login.tsx
+++ src/routes/auth/Login.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Form, ActionFunctionArgs, redirect } from "react-router-dom";
 import { logIn } from "@api/auth";
 
-export async function action({ request, params }: ActionFunctionArgs) {
+export async function action({ request }: ActionFunctionArgs) {
   let formData = await request.formData();
   const { email, password } = Object.fromEntries(formData);
   //? 과연 이런 식의 코드가 파이어 베이스와 유용하게 쓰일 수 있을까라는 의문이 든다.
