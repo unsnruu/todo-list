e5ea5a6e455fc4ee413abee53a7c92affe26d5cc
unsnruu <unsnruu@gmail.com>
2022-10-11 19:47:40 +0900

    ⚰️ 의미없는 주석 제거
    해당 주석에 대한 내용을 이슈로서 README.md에 작성하였습니다.
    따라서 해당 주석을 제거하였습니다.

--- src/routes/auth/Login.tsx
+++ src/routes/auth/Login.tsx
@@ -4,7 +4,7 @@ import { logIn } from "@api/auth";
 export async function action({ request }: ActionFunctionArgs) {
   let formData = await request.formData();
   const { email, password } = Object.fromEntries(formData);
-  //? 과연 이런 식의 코드가 파이어 베이스와 유용하게 쓰일 수 있을까라는 의문이 든다.
+
   if (typeof email !== "string" || typeof password !== "string") return;
 
   await logIn({ email, password });
