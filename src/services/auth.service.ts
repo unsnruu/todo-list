b3bdc3f24386ab4e182b2e95da6296d1427952e8
unsnruu <unsnruu@gmail.com>
2022-10-11 14:12:42 +0900

    🔥 불필요한 메서드 삭제(#10)
    Api 폴더에서도 관리할 수 있는 코드들을 삭제하였습니다.
    firebase의 추상화로 인하여 추가적인 로직없이 api단에서
    관리할 수 있기 때문입니다.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,18 +1,14 @@
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase.service";
 
 //todo: ContextAPI 혹은 Redux로 user정보에 대한 관리?
 //todo: cache를 어떻게 할지?
 const authService = {
-  async signUp(email: string, password: string) {
-    await createUserWithEmailAndPassword(auth, email, password);
+  async isLoggedIn() {
+    onAuthStateChanged(auth, (user) => {
+      if (user) return true;
+      else return false;
+    });
   },
-  async logIn(email: string, password: string) {
-    await signInWithEmailAndPassword(auth, email, password);
-  },
-  async signOut() {},
 };
 export default authService;
