b1fcaa4d7a33d023bb71b51cf2e78955baa34013
unsnruu <unsnruu@gmail.com>
2022-10-12 00:18:36 +0900

    🏷️ emotion의 theme에 대한 type 설정 (#12)
    emotion의 theme에 사용할 컬러들을 type으로 설정하였습니다.

---
+++ src/emotion.d.ts
@@ -0,0 +1,12 @@
+import "@emotion/react";
+
+declare module "@emotion/react" {
+  export interface Theme {
+    color: {
+      primary: "AAC4FF";
+      secondary: "D2DAFF";
+      dark: "B1B2FF";
+      light: "EEF1FF";
+    };
+  }
+}
