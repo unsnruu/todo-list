101e7dec348d825feedda2ddd9b82a486b6868fc
unsnruu <unsnruu@gmail.com>
2022-10-12 18:15:10 +0900

    🚚 라우트 수정 (#15)
    /app 라우트의 컴포넌트로 AppHome을 주었습니다.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,17 +5,20 @@ import {
 } from "react-router-dom";
 
 import Root from "@routes/Root";
+import Home from "@routes/Home";
 import Login, { action as loginAction } from "@routes/auth/Login";
 import SignUp, { action as signupAction } from "@routes/auth/SignUp";
+import AppHome from "@routes/app/AppHome";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
       <Route path="auth">
         <Route path="login" element={<Login />} action={loginAction} />
         <Route path="signup" element={<SignUp />} action={signupAction} />
       </Route>
-      <Route path="app"></Route>
+      <Route path="app" element={<AppHome />}></Route>
     </Route>
   )
 );
