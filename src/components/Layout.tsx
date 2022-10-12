c1867b24fe1c9c45289b370f51909880e1284a87
unsnruu <unsnruu@gmail.com>
2022-10-12 18:12:24 +0900

    💄 Layout에서 AppHome으로 스타일드 컴포넌트 이동 (#15)
    기존에 Layout에서 사용되던 스타일드 컴포넌트를 AppHome으로 이동시켰습니다.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -17,16 +17,3 @@ const Container = styled.div`
   height: 100vh;
   background-color: ${({ theme }) => theme.color.light};
 `;
-const Main = styled.div`
-  display: flex;
-`;
-const Content = styled.div`
-  margin-top: 1rem;
-  margin-right: 1rem;
-  width: 100%;
-  min-height: 20rem;
-  height: 100%;
-  border-radius: 1rem;
-  background-color: transparent;
-  padding: 1rem;
-`;
--- src/routes/app/AppHome.tsx
+++ src/routes/app/AppHome.tsx
@@ -1,9 +1,27 @@
-function Home() {
+import styled from "@emotion/styled";
+import Sidebar from "../../components/Sidebar";
+
+function AppHome() {
   return (
-    <div>
-      <h1>App home</h1>
-    </div>
+    <Main>
+      <Sidebar />
+      <Content></Content>
+    </Main>
   );
 }
 
-export default Home;
+export default AppHome;
+
+const Main = styled.div`
+  display: flex;
+`;
+const Content = styled.div`
+  margin-top: 1rem;
+  margin-right: 1rem;
+  width: 100%;
+  min-height: 20rem;
+  height: 100%;
+  border-radius: 1rem;
+  background-color: transparent;
+  padding: 1rem;
+`;
