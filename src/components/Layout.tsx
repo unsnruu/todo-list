3c6926adb9056df2b92eb619d95620ca6b0326d3
unsnruu <unsnruu@gmail.com>
2022-10-12 17:51:19 +0900

    🔥 Sidebar 컴포넌트를 render에서 제거 (#15)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,16 +2,12 @@ import { PropsWithChildren } from "react";
 import styled from "@emotion/styled";
 
 import Header from "./Header";
-import Sidebar from "./Sidebar";
 
 function Layout({ children }: PropsWithChildren) {
   return (
     <Container>
       <Header />
-      <Main>
-        <Sidebar />
-        <Content>{children}</Content>
-      </Main>
+      {children}
     </Container>
   );
 }
