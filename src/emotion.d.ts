1cc88adc1feafd09ae3212f33d0e1374a86c755e
unsnruu <unsnruu@gmail.com>
2022-10-12 13:18:58 +0900

    🐛 컬러 값에 #을 붙이지 않아서 스타일링이 되지 않는 문제 해결 (#12)

--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -3,10 +3,10 @@ import "@emotion/react";
 declare module "@emotion/react" {
   export interface Theme {
     color: {
-      primary: "AAC4FF";
-      secondary: "D2DAFF";
-      dark: "B1B2FF";
-      light: "EEF1FF";
+      primary: "#AAC4FF";
+      secondary: "#D2DAFF";
+      dark: "#B1B2FF";
+      light: "#EEF1FF";
     };
   }
 }
