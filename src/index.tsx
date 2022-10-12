95ebe15f9b5d94a9053ae129134269838c08f045
unsnruu <unsnruu@gmail.com>
2022-10-12 13:19:34 +0900

    ✨ emotion의 theme provider 설정 (#12)

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 
+import { ThemeProvider, Theme } from "@emotion/react";
+
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/index";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const theme: Theme = {
+  color: {
+    primary: "#AAC4FF",
+    secondary: "#D2DAFF",
+    dark: "#B1B2FF",
+    light: "#EEF1FF",
+  },
+};
+
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
