2001f253ab0f9cd5c5780a808d394c37f785fbf0
unsnruu <unsnruu@gmail.com>
2022-10-12 16:52:04 +0900

    💄 Main의 스타일 수정 (#15)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,22 +2,28 @@ import { PropsWithChildren } from "react";
 import styled from "@emotion/styled";
 
 import Header from "./Header";
-import Main from "./Main";
 import Sidebar from "./Sidebar";
 
 function Layout({ children }: PropsWithChildren) {
   return (
-    <div>
+    <Container>
       <Header />
       <Main>
         <Sidebar />
         <Content>{children}</Content>
       </Main>
-    </div>
+    </Container>
   );
 }
 export default Layout;
 
+const Container = styled.div`
+  height: 100vh;
+  background-color: ${({ theme }) => theme.color.light};
+`;
+const Main = styled.div`
+  display: flex;
+`;
 const Content = styled.div`
   margin-top: 1rem;
   margin-right: 1rem;
