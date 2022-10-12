a8055952f81a3524412321be1469c9a9e8015497
unsnruu <unsnruu@gmail.com>
2022-10-13 00:00:36 +0900

    ♻️ StyledLink를 추출하여 별도의 컴포넌트로 분리 (#17)

---
+++ src/components/StyledLink.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const StyledLink = styled(Link)`
+  padding: 1rem;
+  border-radius: 1rem;
+  text-decoration: none;
+`;
+
+export default StyledLink;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
+
 import Header from "@components/Header";
+import StyledLink from "@components/StyledLink";
 
 function Home() {
   return (
@@ -21,11 +22,6 @@ const ButtonContainer = styled.div`
   & > a {
     margin-left: 1rem;
   }
-`;
-const StyledLink = styled(Link)`
-  padding: 1rem;
-  border-radius: 1rem;
-  text-decoration: none;
 `;
 const LogInLink = styled(StyledLink)`
   color: ${({ theme }) => theme.color.dark};
