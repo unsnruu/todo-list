37b8689acd5b88ea733eb5c2ec6aa8de164ff6ac
unsnruu <unsnruu@gmail.com>
2022-10-13 00:04:37 +0900

    💄 LogOutButton 스타일드 컴포넌트 생성 (#17)

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -6,7 +6,9 @@ import Sidebar from "@components/Sidebar";
 function AppLayout({ children }: PropsWithChildren) {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <LogOutButton>로그아웃</LogOutButton>
+      </Header>
       <Main>
         <Sidebar />
         <Content>{children}</Content>
@@ -15,19 +17,22 @@ function AppLayout({ children }: PropsWithChildren) {
     </Container>
   );
 }
-
 export default AppLayout;
 
 const Container = styled.div``;
 const Main = styled.div`
   display: flex;
 `;
-
+const LogOutButton = styled.button`
+  border: none;
+  background-color: transparent;
+  font-size: 1rem;
+  color: ${({ theme }) => theme.color.dark};
+`;
 const Content = styled.div`
   margin-top: 1rem;
   margin-right: 1rem;
   width: 100%;
-  min-height: 20rem;
   height: 100%;
   border-radius: 1rem;
   background-color: transparent;
