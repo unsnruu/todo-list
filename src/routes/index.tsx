9c5d0fb5b14e0530ebc4e62862e876e6bf2991cf
unsnruu <unsnruu@gmail.com>
2022-10-12 17:33:00 +0900

    ✨ 새로운 라우트를 생성 (#15)
    auth와 app 라우트를 새롭게 생성하였습니다.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,12 +8,14 @@ import Root from "@routes/app/Root";
 import Login, { action as loginAction } from "@routes/auth/Login";
 import SignUp, { action as signupAction } from "@routes/auth/SignUp";
 
-//todo: auth obeserver로 element 삼항 연산!
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
-      <Route path="/login" element={<Login />} action={loginAction} />
-      <Route path="/signup" element={<SignUp />} action={signupAction} />
+      <Route path="auth">
+        <Route path="login" element={<Login />} action={loginAction} />
+        <Route path="signup" element={<SignUp />} action={signupAction} />
+      </Route>
+      <Route path="app"></Route>
     </Route>
   )
 );
