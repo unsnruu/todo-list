051c483d4603628f6b9757a8206afdde2220afc4
unsnruu <unsnruu@gmail.com>
2022-10-12 22:52:14 +0900

    💄 styled components를 추가 (#17)

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,36 @@
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import Header from "@components/Header";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <Header>
+        <ButtonContainer>
+          <LogInLink to="auth/login">로그인</LogInLink>
+          <SignUpLink to="auth/signup">회원가입</SignUpLink>
+        </ButtonContainer>
+      </Header>
+    </div>
+  );
 }
 
 export default Home;
+
+const ButtonContainer = styled.div`
+  & > a {
+    margin-left: 1rem;
+  }
+`;
+const StyledLink = styled(Link)`
+  padding: 1rem;
+  border-radius: 1rem;
+  text-decoration: none;
+`;
+const LogInLink = styled(StyledLink)`
+  color: ${({ theme }) => theme.color.dark};
+`;
+const SignUpLink = styled(StyledLink)`
+  color: ${({ theme }) => theme.color.light};
+  background-color: ${({ theme }) => theme.color.dark};
+`;
