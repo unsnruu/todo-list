aa69c6840cd142c1e75ab4993b187c987bc266d6
unsnruu <unsnruu@gmail.com>
2022-10-13 11:52:31 +0900

    🔥 isLoggedIn 삭제 (#17)
    isLoggedIn 메서드를 삭제하였습니다.
    1. API와 성격이 다른 함수라고 생각되었기 때문입니다.
    2. 사용할 이유가 없습니다. 왜냐하면 firebase에서 자체적으로 인증된 유저에 대한 값을
    저장소 개념으로서 auth에서 관리하기 때문입니다.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -12,10 +12,6 @@ class AuthService {
   async logIn(email: string, password: string) {
     await signInWithEmailAndPassword(auth, email, password);
   }
-  isLoggedIn() {
-    if (auth.currentUser) return true;
-    else return false;
-  }
   async logOut() {
     await signOut(auth);
   }
