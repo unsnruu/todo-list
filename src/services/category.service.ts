daaaa9ae9a23ff50510a46499e4045dc5d4c765f
unsnruu <unsnruu@gmail.com>
2022-10-15 02:28:48 +0900

    ✨ addCategory 로직 추가 및 전체 로직 다듬기 (#19)
    firebase.service.ts에서 추상화를 시켜 해당 로직을 단순화 시켰습니다.
    또한 auth.service.ts에서 가입과 동시에 category를 만들도록 설정하여,
    addCategory메서드가 정상작동 하도록 수정하였습니다.

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,34 +1,15 @@
-import {
-  addDoc,
-  arrayUnion,
-  doc,
-  getDoc,
-  setDoc,
-  updateDoc,
-} from "firebase/firestore";
-import { db, getUserId } from "./firebase.service";
+import { arrayUnion, getDoc, updateDoc } from "firebase/firestore";
+import { getCollectionRef } from "./firebase.service";
 
-const COLLECTION_CATEGORY = "category";
 const categoryService = {
   async getCategories() {
-    const uid = getUserId();
-    const docRef = doc(db, COLLECTION_CATEGORY, uid);
-    const docSnap = await getDoc(docRef);
-    console.log(docSnap.data());
+    const collectionRef = getCollectionRef();
+    const docSnap = await getDoc(collectionRef);
+    return docSnap.data();
   },
   async addCateogry(newCategory: string) {
-    const uid = getUserId();
-    const docRef = doc(db, COLLECTION_CATEGORY, uid);
-
-    await setDoc(docRef, {
-      /**
-       * 기존 카테고리에 새로 받은 카테고리를 추가하기
-       * 그러러면 기존 카테고리에 대한 정보를 가지고 있어야하지 않나?
-       * arrayUnion이라는 함수를 제공하는 듯 하다.
-       *
-       * 회원 가입시에 최초의 데이터에 대해서는
-       * 만들어내는 작업이 필요할 듯 하다.
-       */
+    const collectionRef = getCollectionRef();
+    await updateDoc(collectionRef, {
       category: arrayUnion(newCategory),
     });
   },
