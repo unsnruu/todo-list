ffc7f5e766b26dd3579228dafac984ae26dffb74
unsnruu <unsnruu@gmail.com>
2022-10-14 13:55:08 +0900

    ✨ auth.currentUser.uid를 가져오는 로직을 함수로 분리 (#19)

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -13,8 +13,13 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+const getUserId = () => {
+  if (!auth.currentUser) throw new Error("유저 정보가 존재하지 않습니다.");
+  return auth.currentUser.uid;
+};
+
 const createCollection = <T = DocumentData>(collectionName: string) => {
   return collection(db, collectionName) as CollectionReference<T>;
 };
 
-export { app, auth, db, createCollection };
+export { app, auth, db, createCollection, getUserId };
