004d5d6fa57c6f1fd55f9875c2e3c02fa2844d6b
unsnruu <unsnruu@gmail.com>
2022-10-15 02:22:26 +0900

    ✨ collectionRef를 반환하는 함수 생성 (#19)
    category collectionRef를 반환하는 함수를 생성하였습니다.

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -7,7 +7,9 @@ import {
   DocumentData,
   CollectionReference,
   collection,
+  doc,
 } from "firebase/firestore";
+import { COLLECTION_CATEGORY } from "src/constant/common";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -17,9 +19,12 @@ const getUserId = () => {
   if (!auth.currentUser) throw new Error("유저 정보가 존재하지 않습니다.");
   return auth.currentUser.uid;
 };
+const getCollectionRef = () => {
+  return doc(db, COLLECTION_CATEGORY, getUserId());
+};
 
 const createCollection = <T = DocumentData>(collectionName: string) => {
   return collection(db, collectionName) as CollectionReference<T>;
 };
 
-export { app, auth, db, createCollection, getUserId };
+export { app, auth, db, createCollection, getUserId, getCollectionRef };
