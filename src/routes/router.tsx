c7b918a00a35d3d3d750603fa4ab63b3c85a3c60
unsnruu <unsnruu@gmail.com>
2022-10-16 16:46:29 +0900

    🔥 AppRoot의 loader 삭제에 따른 import 수정 (#24)

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -8,7 +8,7 @@ import Root from "@routes/Root";
 import Home from "@routes/Home";
 import Login, { action as loginAction } from "@routes/auth/Login";
 import SignUp, { action as signupAction } from "@routes/auth/SignUp";
-import AppRoot, { loader as appRootLoader } from "@routes/app/AppRoot";
+import AppRoot from "@routes/app/AppRoot";
 import AppHome from "@routes/app/AppHome";
 import TodoList, { loader as todoListLoader } from "@routes/app/TodoList";
 
@@ -20,7 +20,7 @@ export const router = createBrowserRouter(
         <Route path="login" element={<Login />} action={loginAction} />
         <Route path="signup" element={<SignUp />} action={signupAction} />
       </Route>
-      <Route path="app" element={<AppRoot />} loader={appRootLoader}>
+      <Route path="app" element={<AppRoot />}>
         <Route index element={<AppHome />} />
         <Route
           path="todo/:category"
