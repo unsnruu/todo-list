97410627fe5b1c1ba80f0b4c2bcf76e3011be57a
unsnruu <unsnruu@gmail.com>
2022-10-17 13:26:53 +0900

    ✨ action 함수 생성 및 스타일드 컴포넌트 생성 (#26)
    Form의 request를 위한 action 함수를 생성하고 구현하였습니다.
    해당 action 함수는 post문에 대해서만 작동하고 이후에
    edit과 delete를 사용할 경우 로직을 추가하도록 하겠습니다.

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,29 @@
-function TodoItem() {
-  return <div></div>;
+import styled from "@emotion/styled";
+
+interface TodoItemProps {
+  text: string;
+  handleDelete: () => void;
+  handleEdit: () => void;
 }
 
+function TodoItem({ text, handleDelete }: TodoItemProps) {
+  return (
+    <Container>
+      <Text>{text}</Text>
+      <div>
+        <span onClick={handleDelete}>지우기</span>
+      </div>
+    </Container>
+  );
+}
 export default TodoItem;
+
+const Container = styled.li`
+  background-color: white;
+  margin-bottom: 1rem;
+  list-style: none;
+  padding: 1rem;
+  border-radius: 0.5rem;
+`;
+
+const Text = styled.span``;
