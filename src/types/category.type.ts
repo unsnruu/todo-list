7fa7c8b799abd267d6610648c06cbc12e818316f
unsnruu <unsnruu@gmail.com>
2022-10-20 19:48:22 +0900

    🏷️ categoryService 타입 지정 (#29)

--- src/types/category.type.ts
+++ src/types/category.type.ts
@@ -1,9 +1,9 @@
-import { Categories } from "src/constant/common";
+import { Categories, Category } from "src/constant/common";
 
 export interface CategoryService {
   categories: Categories | null;
   getCategories(): Promise<void>;
-  addCategory(): Promise<void>;
+  addCategory(newCategory: Category): Promise<void>;
   editCategory(): Promise<void>;
   deleteCategory(): Promise<void>;
 }
