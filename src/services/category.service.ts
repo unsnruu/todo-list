4b8597ae1d6cc126dca73034d062915847f9c995
unsnruu <unsnruu@gmail.com>
2022-10-20 19:48:58 +0900

    ♻️ CategoryService 인터페이스 구현 (#29)
    기존에 Object 형태로 있었던 CategoryService를
    인터페이스로 정의한 CategoryService를 통해서
    CategoryServiceImpl로 구현하였습니다.

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,20 +1,31 @@
 import { arrayUnion, getDoc, updateDoc } from "firebase/firestore";
 import { getCollectionRef } from "./firebase.service";
+import { CategoryService } from "../types/category.type";
+import { Categories } from "src/constant/common";
 
-const categoryService = {
-  async getCategories() {
+class CategoryServiceImpl implements CategoryService {
+  categories: Categories | null;
+  constructor() {
+    this.categories = null;
+  }
+  async getCategories(): Promise<void> {
     const collectionRef = getCollectionRef();
     const docSnap = await getDoc(collectionRef);
     if (docSnap.exists()) {
-      return docSnap.data();
+      const { categories } = docSnap.data();
+      if (!categories) {
+        throw new Error("서버측으로부터 받은 카테고리가 없습니다");
+      }
+      this.categories = categories;
     }
-  },
-  async addCateogry(newCategory: string) {
+  }
+  async addCategory(newCategory: string): Promise<void> {
     const collectionRef = getCollectionRef();
     await updateDoc(collectionRef, {
       categories: arrayUnion(newCategory),
     });
-  },
-};
-
-export default categoryService;
+  }
+  async editCategory(): Promise<void> {}
+  async deleteCategory(): Promise<void> {}
+}
+export default new CategoryServiceImpl();
