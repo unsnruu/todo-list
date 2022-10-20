0b6cf48290aff6fdbbb0a40cf0c2692040ce0317
unsnruu <unsnruu@gmail.com>
2022-10-20 12:17:29 +0900

    🔥 불필요한 테스트 코드 삭제 (#26)

--- src/routes/app/AppHome.tsx
+++ src/routes/app/AppHome.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
 
 function AppHome() {
   useEffect(() => {}, []);
@@ -7,7 +6,6 @@ function AppHome() {
   return (
     <div>
       <h1>Home</h1>
-      <Link to="todo/test">go to test</Link>
     </div>
   );
 }
