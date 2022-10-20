903b162dd7870ca8051f05b90465fa5498476c43
unsnruu <unsnruu@gmail.com>
2022-10-20 22:36:57 +0900

    ✏️ import 문의 오타 수정 (#29)

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import { Global, ThemeProvider, Theme, css } from "@emotion/react";
 
 import { router } from "@routes/router";
-import RootProvider from "@context/RootContext";
+import RootProvider from "@context/rootContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
