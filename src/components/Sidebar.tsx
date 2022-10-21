90a652468b89cd4a935c194c667b851b62fac85d
unsnruu <unsnruu@gmail.com>
2022-10-21 13:53:37 +0900

    🔥 업데이트에 따른 파일 삭제 (#29)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,8 @@
 import styled from "@emotion/styled";
-import useCategory from "@hooks/useCategory";
+
 import StyledLink from "@components/StyledLink";
-import type { Category } from "../types/category.type";
 import useTodosState from "@hooks/useTodoList";
+import type { Category } from "../types/category.type";
 
 function Sidebar() {
   const { categories, setSelectedCategory } = useTodosState();
