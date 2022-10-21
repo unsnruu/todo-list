013044d80eb48860a992c069500ef79984ccbe79
unsnruu <unsnruu@gmail.com>
2022-10-21 13:56:50 +0900

    🔥 categoryContext 삭제에 따른 훅 삭제 (#29)

--- src/hooks/useCategory.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { CategoryContext } from "../context/categoryContext";
-
-export default function useCategory() {
-  return useContext(CategoryContext);
-}
