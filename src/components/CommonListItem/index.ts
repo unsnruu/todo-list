08f66b4572a1e9bfc9c46f25abff8b0cd1262692
unsnruu <unsnruu@gmail.com>
2022-10-24 03:12:49 +0900

    ✨ 재사용성 향상을 위한 Item 컴포넌트 개발 (#23)
    CategoryEdit 라우트와 TodoList 라우트에서 거의 동일한 UI를 사용하기 때문에,
    재사용성을 높이기 위하여 해당 컴포넌트를 잘게 쪼개 합성 컴포넌트로 구성할 계획입니다.

---
+++ src/components/CommonListItem/index.ts
@@ -0,0 +1,12 @@
+import Main from "./Main";
+import Completion from "./Completion";
+import Text from "./Text";
+import Icons from "./Icons";
+
+const CommonListItem = Object.assign(Main, {
+  Completion,
+  Text,
+  Icons,
+});
+
+export default CommonListItem;
