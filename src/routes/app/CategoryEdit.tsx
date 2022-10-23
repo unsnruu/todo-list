0f3fed7547f719d7cd43a5910f919b01d29d84d5
unsnruu <unsnruu@gmail.com>
2022-10-23 12:25:39 +0900

    ✨ Category 타입 변경에 따른 렌더 로직 변경 (#23)
    Category 타입을 변경함에 따라서 기존에 string 타입이었던 카테고리를 바로
    렌더링 하던 로직을 구조분해 할당을 통해 title 값만 렌더링 하도록 변경하였습니다.

--- src/routes/app/CategoryEdit.tsx
+++ src/routes/app/CategoryEdit.tsx
@@ -1,5 +1,30 @@
+import { MdModeEdit } from "react-icons/md";
+
+import useTodoList from "@hooks/useTodoList";
+import categoryService from "@services/category.service";
+
 function CategoryEdit() {
-  return <div>CategoryEdit</div>;
+  const { categories } = useTodoList();
+
+  const handleClickEdit = () => {
+    //카테고리 서버상에서 수정
+    categoryService.editCategory();
+    //카테고리 로컬상에서 수정
+  };
+
+  return (
+    <div>
+      <h1>카테고리 수정</h1>
+      <ul>
+        {categories?.map(({ title }, idx) => (
+          <li key={idx}>
+            <span>{title}</span>
+            <MdModeEdit onClick={handleClickEdit} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default CategoryEdit;
