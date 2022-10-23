f4251fc7e8870f46ac56f77925b17c5bc6b272d4
unsnruu <unsnruu@gmail.com>
2022-10-23 14:53:17 +0900

    🏷️ TodoService에 메서드를 의존성 주입 형태로 변경 (#23)
    TodoService의 메서드들이 User 객체 다루는 방식을 변경하였습니다.
    인자로 받는 의존성 주입의 형태를 띄도록 변경하였습니다.
    firebase의 유저 정보를 곧바로 사용하지 않는 이유는 user 정보를 받아오는데 시간이
    걸리기 때문입니다. 따라서 유저 정보가 제대로 된 값을 받았는지 확인하기 어렵기 떄문입니다.
    만일 컴포넌트 단에서 해당 유저 정보를 받아올 경우 user 정보가 존재하므로
    별도의 validation이 필요하지 않기때문에 다음과 같은 형태로 코드를 짰습니다.

--- src/types/todo.type.ts
+++ src/types/todo.type.ts
@@ -1,5 +1,6 @@
-import type { Text, UserId, TodoId } from "./common.type";
+import type { Text, TodoId } from "./common.type";
 import type { Category } from "../types/category.type";
+import { User } from "./user.type";
 
 export interface Todo {
   text: Text;
@@ -10,9 +11,10 @@ export interface Todo {
 export type Todos = Todo[];
 export type TodoForm = Omit<Todo, "id">;
 
+//user을 TodoService가 품는 방법은 없을까방
 export interface TodoService {
-  getTodosByCategory(category: Category): Promise<Todos>;
-  addTodo(newTood: TodoForm): Promise<void>;
-  editTodo(id: TodoId, todo: TodoForm): Promise<void>;
-  deleteTodo(id: TodoId): Promise<void>;
+  getTodosByCategory(title: string, user: User): Promise<Todos>;
+  addTodo(newTodo: string, user: User): Promise<void>;
+  editTodo(id: TodoId, todo: TodoForm, user: User): Promise<void>;
+  deleteTodo(id: TodoId, user: User): Promise<void>;
 }
