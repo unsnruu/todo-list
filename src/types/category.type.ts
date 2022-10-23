36cb7f6ab0950a63e8aa33bbd764929c88c6628b
unsnruu <unsnruu@gmail.com>
2022-10-23 12:23:56 +0900

    🏷️ Category 타입 수정 (#23)

--- src/types/category.type.ts
+++ src/types/category.type.ts
@@ -1,11 +1,11 @@
 import { User } from "./user.type";
 
-export type Category = string;
+export type Category = { id: string; title: string };
 export type Categories = Category[] | null;
 
 export interface CategoryService {
   getCategories(user: User): Promise<Categories | null>;
-  addCategory(user: User, newCategory: Category): Promise<void>;
+  addCategory(user: User, newCategory: string): Promise<void>;
   editCategory(): Promise<void>;
   deleteCategory(): Promise<void>;
 }
