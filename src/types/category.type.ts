ec22f677f82dd9fd846513e9a4f47609f0d25833
unsnruu <unsnruu@gmail.com>
2022-10-25 16:37:49 +0900

    ✨ editCategory 메서드의 인자 정의 (#23)

--- src/types/category.type.ts
+++ src/types/category.type.ts
@@ -1,17 +1,28 @@
+import { Todos } from "./todo.type";
 import { User } from "./user.type";
 
 export type CategoryTitle = string;
 export type CategoryId = string;
 
-export type Category = { id: CategoryId; title: CategoryTitle };
+export type Category = { id: string; title: string };
 export type Categories = Category[];
 
 export interface CategoryService {
   getCategories(user: User): Promise<Categories | null>;
   addCategory(
-    newCategory: CategoryTitle,
+    newCategoryTitle: CategoryTitle,
     user: User
   ): Promise<CategoryId | null>;
-  editCategory(): Promise<void>;
+  editCategory(
+    category: Category,
+    newTitle: CategoryTitle,
+    user: User
+  ): Promise<void>;
   deleteCategory(category: Category, user: User): Promise<CategoryId>;
 }
+
+/**
+ * edit에 대해서
+ * 1. 수정을 가하기 위해서는 이전 카테고리의 정보와 바꾸려는 카테고리의 정보가 모두 필요하다.
+ * 2.
+ */
