3a0d20f7b4721fd798b330f55477abe42aacc4ce
unsnruu <unsnruu@gmail.com>
2022-10-26 20:07:33 +0900

    💄 Skeleton에 애니메이션 추가 (#28)
    Skeleton에 애니메이션을 추가하였습니다. 또한 스켈레톤에 특정한 값을 넣어서
    사용하는 방식이 아니라 이를 container 감싸서 사용하는 식으로 크기를 조정하는
    방식을 채택하였습니다.

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,35 +1,22 @@
 import styled from "@emotion/styled";
 import { keyframes } from "@emotion/react";
 
-interface SkeletonProps {
-  width: number;
-  wUnit: string;
-  height: number;
-  hUnit: string;
-}
-
-function Skeleton({
-  width,
-  height,
-  hUnit = "px",
-  wUnit = "px",
-}: SkeletonProps) {
-  return (
-    <Container
-      width={width}
-      wUnit={wUnit}
-      height={height}
-      hUnit={hUnit}
-    ></Container>
-  );
+function Skeleton() {
+  return <Container />;
 }
 export default Skeleton;
 
 const movement = keyframes`
-
+  0% {
+    background-color: hsl(200, 20%, 80%);
+  }
+  100% {
+    background-color: hsl(200, 20%, 95%);
+  }
 `;
 
-const Container = styled.div<SkeletonProps>`
-  width: ${({ width, wUnit }) => `${width}${wUnit}`};
-  height: ${({ height, hUnit }) => `${height}${hUnit}`};
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  animation: ${movement} 1s ease;
 `;
