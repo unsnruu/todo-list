5bc694d4d09b2597ebcf0f74c9bf9aa29bc804b1
unsnruu <unsnruu@gmail.com>
2022-11-01 19:02:35 +0900

    💄 SignUp 컴포넌트의 스타일 수정 (#46)
    Login 컴포넌트의 스타일링을 복사하여 SignUp에 적용하였습니다.

--- src/routes/auth/SignUp.tsx
+++ src/routes/auth/SignUp.tsx
@@ -1,24 +1,32 @@
-import authService from "@services/auth.service";
+import styled from "@emotion/styled";
 import { ActionFunctionArgs, Form, redirect } from "react-router-dom";
+
+import authService from "@services/auth.service";
+import ShadowBox from "@components/ShadowBox";
 
 function SignUp() {
   return (
-    <div>
-      <h1>회원가입</h1>
-      <Form aria-label="회원가입" method="post">
-        <label>
-          이메일
-          <input type="text" name="email" />
-        </label>
-        <label>
-          비밀번호
-          <input type="password" name="password" />
-        </label>
-        <button>제출</button>
-      </Form>
-    </div>
+    <Container>
+      <ShadowBox>
+        <h1>로그인</h1>
+        <StyledForm method="post" aria-label="로그인">
+          <label>
+            이메일
+            <br />
+            <input type="text" name="email" />
+          </label>
+          <label>
+            비밀번호
+            <br />
+            <input type="password" name="password" />
+          </label>
+          <button>제출</button>
+        </StyledForm>
+      </ShadowBox>
+    </Container>
   );
 }
+export default SignUp;
 
 export async function action({ request }: ActionFunctionArgs) {
   let formData = await request.formData();
@@ -29,4 +37,36 @@ export async function action({ request }: ActionFunctionArgs) {
   return redirect("/");
 }
 
-export default SignUp;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledForm = styled(Form)`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  & input {
+    border: none;
+    background-color: transparent;
+    margin-bottom: 1rem;
+    font-size: 1.25rem;
+  }
+  & label {
+    font-size: 1.25rem;
+    margin-bottom: 1rem;
+  }
+
+  & input:focus {
+    outline: none;
+  }
+  & button {
+    border: none;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    background-color: ${({ theme }) => theme.color.primary};
+    color: white;
+    cursor: pointer;
+  }
+`;
